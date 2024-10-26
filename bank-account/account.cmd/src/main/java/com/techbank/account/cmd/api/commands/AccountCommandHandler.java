76f693e9afffb2bc97da53822f2846c4a16a5196
nicoxiang <916592561@qq.com>
2024-10-26 18:01:32 +0800

    command handler

---
+++ bank-account/account.cmd/src/main/java/com/techbank/account/cmd/api/commands/AccountCommandHandler.java
@@ -0,0 +1,43 @@
+package com.techbank.account.cmd.api.commands;
+
+import com.techbank.account.cmd.domain.AccountAggregate;
+import com.techbank.cqrs.core.handlers.EventSouringHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountCommandHandler implements CommandHandler{
+
+    @Autowired
+    private EventSouringHandler<AccountAggregate> eventSouringHandler;
+
+    @Override
+    public void handle(OpenAccountCommand command) {
+        AccountAggregate aggregate = new AccountAggregate(command);
+        eventSouringHandler.save(aggregate);
+    }
+
+    @Override
+    public void handle(DepositFundsCommand command) {
+        AccountAggregate aggregate = eventSouringHandler.getById(command.getId());
+        aggregate.depositFunds(command.getAmount());
+        eventSouringHandler.save(aggregate);
+    }
+
+    @Override
+    public void handle(WithdrawFundsCommand command) {
+        AccountAggregate aggregate = eventSouringHandler.getById(command.getId());
+        if(command.getAmount() > aggregate.getBalance()) {
+            throw new IllegalStateException("Withdrawal declined, insufficient funds!");
+        }
+        aggregate.withdrawFunds(command.getAmount());
+        eventSouringHandler.save(aggregate);
+    }
+
+    @Override
+    public void handle(CloseAccountCommand command) {
+        AccountAggregate aggregate = eventSouringHandler.getById(command.getId());
+        aggregate.closeAccount();
+        eventSouringHandler.save(aggregate);
+    }
+}
